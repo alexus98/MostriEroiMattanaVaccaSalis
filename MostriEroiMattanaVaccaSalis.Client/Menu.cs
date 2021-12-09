cd10d3a3c3b12cff5bc9603bd3a0b9ab0d351114
Alessandro Vacca <alessandro.vacca@avanade.com>
2021-12-09 11:52:25 +0100

    User Admin Menu

--- MostriEroiMattanaVaccaSalis.Client/Menu.cs
+++ MostriEroiMattanaVaccaSalis.Client/Menu.cs
@@ -17,12 +17,68 @@ namespace MostriEroiMattanaVaccaSalis.Client
 
         internal static void AdminMenu()
         {
+            char choice;
+            do
+            {
+                Console.WriteLine("Scegli 1 per Giocare\n+" +
+                "Scegli 2 per Creare un nuovo eroe\n" +
+                "Scegli 3 per Eliminare un eroe\n" +
+                "Scegli 4 per Creare un nuovo mostro\n" +
+                "Scegli 5 per Mostrare la classifica globale\n" +
+                "Scegli Q per Uscire");
 
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        break;
+                    case 'Q':
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non valida");
+                        break;
+                }
+            } while (choice != 'Q');
         }
 
         internal static void UserMenu()
         {
+            char choice;
+            do
+            {
+                Console.WriteLine("Scegli 1 per Giocare\n+" +
+                "Scegli 2 per Creare un nuovo eroe\n" +
+                "Scegli 3 per Eliminare un eroe\n" +
+                "Scegli Q per Uscire");
 
+                choice = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case 'Q':
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non valida");
+                        break;
+                }
+            } while (choice != 'Q');
         }
     }
 }
